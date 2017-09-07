78f69031d7aeb86a692763fdd6ac3369e72e3569
David Chen <qydchen@gmail.com>
2017-09-07 02:33:16 -0400

    DRY listsum

--- listSum.js
+++ listSum.js
@@ -5,14 +5,14 @@ class ListNode {
   }
 }
 
-var addTwoNumbers = function(l1, l2) {
-  var result = [];
-  var resultArr = [];
-  l1 = l1 ? l1 : new ListNode(0)
-  l2 = l2 ? l2 : new ListNode(0)
-  var sum = l1.val + l2.val;
+let addTwoNumbers = function(l1, l2) {
+  let result = [];
+  let resultArr = [];
+  l1 = l1 ? l1 : new ListNode(0);
+  l2 = l2 ? l2 : new ListNode(0);
+  let sum = l1.val + l2.val;
   result.push(sum % 10);
-  var carry = Math.floor(sum / 10)
+  let carry = Math.floor(sum / 10);
   if (l1.next) {
     l1.next.val = l1.next.val + carry;
     resultArr = addTwoNumbers(l1.next, l2.next);
@@ -26,11 +26,11 @@ var addTwoNumbers = function(l1, l2) {
 };
 
 
-var one = new ListNode(2)
+let one = new ListNode(2)
 one.next = new ListNode(5)
 one.next.next = new ListNode(3)
 
-var two = new ListNode(2)
+let two = new ListNode(2)
 two.next = new ListNode(5)
 two.next.next = new ListNode(3)
 two.next.next.next = new ListNode(8)
@@ -38,5 +38,5 @@ two.next.next.next.next = new ListNode(9)
 
 console.log(addTwoNumbers(one, two))
 
-var five = new ListNode(5)
+let five = new ListNode(5)
 console.log(addTwoNumbers(five, five))
