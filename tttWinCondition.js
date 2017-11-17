8302a8633f5bfb9a1b194095b025aabf57df44b5
David Chen <qydchen@gmail.com>
2017-11-16 21:15:37 -0500

    refactor

--- tttWinCondition.js
+++ tttWinCondition.js
@@ -19,46 +19,41 @@ let board4 = [
 ];
 
 function isSolved(board) {
+  const possibleWins = getPossibleWins(board);
   let stillPlaying = null;
-  let winConditions = getWinningConditions(board);
-  for (let i = 0; i < winConditions.length; i++) {
-    let winCondition = winConditions[i];
-    let possibleWin = winCondition.map((coord) => board[coord[0]][coord[1]]);
+  for (let i = 0; i < possibleWins.length; i++) {
+    const possibleWin = possibleWins[i];
     if (!stillPlaying) {
-      stillPlaying = possibleWin.some((mark) => mark === "");
+      stillPlaying = possibleWin.some(mark => mark === "");
     }
-    let xWon = possibleWin.every((mark) => mark === 'X');
-    let oWon = possibleWin.every((mark) => mark === 'O');
+    const xWon = possibleWin.every(mark => mark === 'X');
+    const oWon = possibleWin.every(mark => mark === 'O');
     if (xWon) {
       return 'X';
     } else if (oWon) {
       return 'O';
     }
   }
-  if (stillPlaying) {
-    return 'Still in progress';
-  } else {
-    return 'Draw';
-  }
+  return stillPlaying ? 'Still in progress' : 'Draw';
 }
 
-function getWinningConditions(board) {
-  let horizontal = [[],[],[]];
-  let vertical = [[],[],[]];
-  let downdiag = [];
-  let updiag = [];
+function getPossibleWins(board) {
+  const getMark = (row, col) => board[row][col];
+  const horizontal = [[],[],[]];
+  const vertical = [[],[],[]];
+  const downdiag = [];
+  const updiag = [];
   for (let i = 0; i < board.length; i++) {
-    let row = board[i];
-    downdiag.push([i,i])
-    updiag.push([row.length - 1 - i, i])
-    for (let j = 0; j < row.length; j++) {
-      let cell = row[j];
-      horizontal[i].push([i,j]);
-      vertical[i].push([j,i]);
+    downdiag.push(getMark(i, i));
+    updiag.push(getMark(board.length - 1 - i, i));
+    for (let j = 0; j < board.length; j++) {
+      horizontal[i].push(getMark(i, j));
+      vertical[i].push(getMark(j, i));
     }
   }
   return horizontal.concat(vertical, [updiag, downdiag]);
 }
+
 
 console.log(isSolved(board1));
 console.log(isSolved(board2));
