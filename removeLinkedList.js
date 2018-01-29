f61dd43186baebd28ea571fecfbd2b6195f2dcff
qydchen <qydchen@gmail.com>
2018-01-29 02:14:03 -0500

    remove linked list element

---
+++ removeLinkedList.js
@@ -0,0 +1,5 @@
+const removeElements = function(head, val) {
+    if (head === null) return null;
+    head.next = removeElements(head.next, val)
+    return head.val === val ? head.next : head;
+};
