67279760b855a7ff8a048134ba04be93c4b08c50
qydchen <qydchen@gmail.com>
2018-01-29 01:24:38 -0500

    submit removeElement

---
+++ removeElement.js
@@ -0,0 +1,12 @@
+const removeElement = function(nums, val) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+};
+
+console.log(removeElement([3,2,2,3], 3)); // 2
