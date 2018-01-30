90f3426d10cde38ca53f6f2b89155a4a4a055beb
qydchen <qydchen@gmail.com>
2018-01-30 17:20:51 -0500

    search insert

--- searchInsert.js
+++ searchInsert.js
@@ -20,18 +20,18 @@
 // Output: 0
 
 const searchInsert = function(nums, target) {
-    for (const i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
-            return i;
-        }
-        if (i === 0 && target < nums[0]) {
-            return 0;
-        }
-        if (target > nums[i] && target < nums[i + 1]) {
-            return i + 1;
-        }
-        if (i === nums.length - 1 && target > nums[i]) {
-            return nums.length;
-        }
+  for (const i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      return i;
     }
+    if (i === 0 && target < nums[0]) {
+      return 0;
+    }
+    if (target > nums[i] && target < nums[i + 1]) {
+      return i + 1;
+    }
+    if (i === nums.length - 1 && target > nums[i]) {
+      return nums.length;
+    }
+  }
 };
