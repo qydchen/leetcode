a944065b603b2754c15b6dad9507c973365b062c
qydchen <qydchen@gmail.com>
2018-01-30 17:02:47 -0500

    removeduplicates

---
+++ removeDuplicates.js
@@ -0,0 +1,16 @@
+// Given a shorted array, remove the duplicates in-place such that each
+// element appear only once and return the new length.
+
+
+// O(n) time where n is the length of the array,
+// O(1) space
+const removeDuplicates = function(nums) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
