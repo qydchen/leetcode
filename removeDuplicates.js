deec44e5f17f7091929e972d6801a1f087a5dcd9
qydchen <qydchen@gmail.com>
2018-01-30 17:04:53 -0500

    removeduplicates

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -5,12 +5,12 @@
 // O(n) time where n is the length of the array,
 // O(1) space
 const removeDuplicates = function(nums) {
-    let i = 0;
-    for (let j = 0; j < nums.length; j++) {
-        if (nums[j] !== nums[i]) {
-            i++;
-            nums[i] = nums[j];
-        }
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
     }
-    return i + 1;
+  }
+  return i + 1;
 };
