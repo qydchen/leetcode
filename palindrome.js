9920403a9d52b39db8a220bca28f20142f9ed04a
qydchen <qydchen@gmail.com>
2018-02-18 03:17:17 -0500

    valid palindrome

--- palindrome.js
+++ palindrome.js
@@ -2,7 +2,7 @@ const isPalindrome = function(s) {
   let p = s.toLowerCase().replace(/[^\w]|_/g,'');
   for (let i = 0; i < Math.floor(p.length / 2); i++) {
     if (p[i] !== p[p.length - 1 - i]) {
-        return false;
+      return false;
     }
   }
   return true;
