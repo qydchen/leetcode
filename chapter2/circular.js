d7ccc0a54c417b7c9e98f078940ded0a1c895a7c
qydchen <qydchen@gmail.com>
2018-02-23 15:26:12 -0500

    submit circular

--- chapter2/circular.js
+++ chapter2/circular.js
@@ -4,10 +4,80 @@
 class ListNode {
   constructor(val) {
     this.val = val;
-    this.next = next;
+    this.next = null;
+  }
+
+  print() {
+    this._print(this);
+  }
+
+  _print(node) {
+    while (node !== null) {
+      console.log(node.val);
+      node = node.next;
+    }
   }
 }
 
 function checkLoop(node) {
-  
+  let slow;
+  let fast;
+  if (node !== null) {
+    slow = node;
+    fast = node;
+  } else {
+    return false;
+  }
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (fast === slow) {
+      return true;
+    }
+  }
+  return false;
 }
+
+
+let a = new ListNode('a');
+let b = new ListNode('b');
+let c = new ListNode('c');
+let d = new ListNode('d');
+let e = new ListNode('e');
+let f = new ListNode('f');
+let x = new ListNode('x');
+let y = new ListNode('y');
+let z = new ListNode('z');
+
+// a b c d e f x y z e
+
+a.next = b;
+a.next.next = c;
+a.next.next.next = d;
+a.next.next.next.next = e;
+a.next.next.next.next.next = f;
+a.next.next.next.next.next.next = x;
+a.next.next.next.next.next.next.next = y;
+a.next.next.next.next.next.next.next.next = z;
+a.next.next.next.next.next.next.next.next.next = e;
+console.log(checkLoop(a));
+
+a.next = b;
+a.next.next = null;
+console.log(checkLoop(a));
+
+a.next = b;
+a.next.next = c;
+a.next.next.next = null
+console.log(checkLoop(a));
+
+a.next = b;
+a.next.next = c;
+a.next.next.next = d;
+a.next.next.next.next = e;
+a.next.next.next.next.next = f;
+a.next.next.next.next.next.next = x;
+a.next.next.next.next.next.next.next = y;
+a.next.next.next.next.next.next.next.next = e;
+console.log(checkLoop(a));
