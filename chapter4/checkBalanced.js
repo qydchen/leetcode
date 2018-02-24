404a0399e44febbb2e2947c7eba938612d9f99f1
qydchen <qydchen@gmail.com>
2018-02-24 14:21:52 -0500

    review checkBalanced

--- chapter4/checkBalanced.js
+++ chapter4/checkBalanced.js
@@ -3,6 +3,29 @@
 // the two subtrees of any node never differ by more than one.
 const { ans1, ans2, ans3, ans4, ans5, TreeNode } = require('./minimalTree');
 
-function checkBalanced(tree) {
-
+// O(n) time
+// O(h) space, where h is the height of the tree
+function checkHeight(root) {
+  if (root === null) return -1;
+  const leftHeight = checkHeight(root.left);
+  if (leftHeight === Number.MIN_VALUE) return Number.MIN_VALUE;
+  const rightHeight = checkHeight(root.right);
+  if (rightHeight === Number.MIN_VALUE) return Number.MIN_VALUE;
+  const heightDiff = leftHeight - rightHeight;
+  if (Math.abs(heightDiff) > 1) {
+    return Number.MIN_VALUE;
+  } else {
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
 }
+
+function checkBalanced(tree) {
+  return checkHeight(tree) !== Number.MIN_VALUE;
+}
+
+
+console.log(checkBalanced(ans1));
+// console.log(checkBalanced(ans2));
+// console.log(checkBalanced(ans3));
+// console.log(checkBalanced(ans4));
+// console.log(checkBalanced(ans5));
