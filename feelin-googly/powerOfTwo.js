1fd1735f507b85cb19425d68c0f6f1bfb54589f7
qydchen <qydchen@gmail.com>
2019-02-26 20:36:45 -0500

    submit power of three

--- feelin-googly/powerOfTwo.js
+++ feelin-googly/powerOfTwo.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 // Given an integer, write a function to deteremine if it is a power of two.
 
-const isPowerOfTwo = (n) => {
+const isPowerOfTwo = (n) => { // O(m) time where m is the number of times 'compare' has to exponentiate; O(1) space
     let compare = 1;
     while (compare < n) {
         compare *= 2;
