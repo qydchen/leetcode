49fafa6a69c2f72928222053fdaec8f163e0eaf2
qydchen <qydchen@gmail.com>
2019-02-26 20:28:51 -0500

    submit power of two

--- feelin-googly/powerOfTwo.js
+++ feelin-googly/powerOfTwo.js
@@ -2,5 +2,14 @@ const assert = require('assert');
 // Given an integer, write a function to deteremine if it is a power of two.
 
 const isPowerOfTwo = (n) => {
-
+    let compare = 1;
+    while (compare < n) {
+        compare *= 2;
+    }
+    return compare === n;
 };
+
+assert.equal(isPowerOfTwo(1), true);
+assert.equal(isPowerOfTwo(16), true);
+assert.equal(isPowerOfTwo(218), false);
+assert.equal(isPowerOfTwo(-1), false);
