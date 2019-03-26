de604e76521768320af79c2273e39bd72d14e85a
qydchen <qydchen@gmail.com>
2019-03-25 21:44:27 -0400

    attempt trappingRainWater

--- feelin-googly/arraysAndStrings/trappingRainWater.js
+++ feelin-googly/arraysAndStrings/trappingRainWater.js
@@ -12,3 +12,26 @@ Example:
 Input: [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 Output: 6
 */
+const trap = (height) => {
+    let highest = null;
+    let count = 0;
+    for (let i = 0; i < height.length; i++) {
+        let current = height[i];
+        if (highest === null) {
+            highest = current;
+        }
+        for (let j = i + 1; j < height.length; j++) {
+            let leading = height[j];
+            if (highest > leading) {
+                count += highest - leading;
+            } else {
+                highest = leading;
+                i = j;
+                break;
+            }
+        }
+    }
+    return count;
+};
+
+assert.equal(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6);
