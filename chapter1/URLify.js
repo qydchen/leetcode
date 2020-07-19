14daa65c08620928b377b6ae442d139954b447e1
David Chen <qydchen@gmail.com>
2020-07-19 15:09:09 -0400

    clean up and correct URLify

--- chapter1/URLify.js
+++ chapter1/URLify.js
@@ -1,32 +1,34 @@
-// Write a method to replace all sapces in a string with '%20'. You may assume
+const assert = require('assert');
+// Write a method to replace all spaces in a string with '%20'. You may assume
 // that the string has sufficient space at the end to hold the additional characters,
 // and that you are given the 'true' length of the string.
 
+// Time Complexity: O(n) time where n the number of iterations across each character in the input str
+// Space Complexity: O(n) space where n is correlated to the size of the input str
 
-// Solution 1: creating a new variable
-// O(n) time where n is the length parameter
-// O(1) space
 function URLify(str, length) {
-  let url = '';
-  for (let i = 0; i < length; i++) {
-    let char = str[i];
-    if (char === ' ') {
-      url += '%20';
-    } else {
-      url += char;
+  let result = '';
+  let nonSpaces = 0;
+  Array.from(str).forEach(char => {
+    if (char !== ' ') {
+      nonSpaces += 1;
+    }
+  });
+  let spaces = length - nonSpaces;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] !== ' ') {
+      result += str[i];
+    }
+    if (str[i] === ' ' && spaces > 0) {
+      result += '%20';
+      spaces -= 1;
     }
   }
-  return url;
+  return result;
 }
 
-// console.log(URLify('Mr John Smith     ', 13)) // => 'Mr%20John%20Smith'
-
-// Solution 2: modifying the parameter str
-// O(n) time to split and then remove the empty strings in reverse
-// O(1) space
-function URLify2(str, length) {
-  str = str.slice(0, length);
-  return str = str.split(' ').join('%20');
-}
-
-console.log(URLify2('Mr John Smith     ', 13)) // => 'Mr%20John%20Smith'
+assert.equal(URLify('Mr John Smith     ', 13), 'Mr%20John%20Smith');
+assert.equal(URLify('Mr John Smith     ', 14), 'Mr%20John%20Smith%20');
+assert.equal(URLify(' hello ', 6), '%20hello');
+assert.equal(URLify('hel lo', 5), 'hello');
+assert.equal(URLify('', 0), '');
