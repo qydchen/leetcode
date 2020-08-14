a95c779d3d9cf32d6128f1b535a4581513ebc681
David Chen <qydchen@gmail.com>
2020-08-14 10:31:46 -0400

    clean graphSearch"

--- chapter4/graphSearch.js
+++ chapter4/graphSearch.js
@@ -1,50 +1,52 @@
 class Graph {
-  constructor(nodes) {
-    this.nodes = nodes;
-  }
+    constructor(nodes) {
+        this.nodes = nodes;
+    }
 }
 
 class GraphNode {
-  constructor(name) {
-    this.name = name;
-    this.children = [];
-    this.visited = false;
-  }
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+        this.visited = false;
+    }
 }
 
 function dfSearch(root) {
-  if (!root) return;
-  console.log(root.name);
-  root.visited = true;
-  root.children.forEach(n => {
-    if (!n.visited) {
-      dfSearch(n);
-    }
-  });
+    if (!root) return;
+    console.log(root.name);
+    root.visited = true;
+    root.children.forEach((n) => {
+        if (!n.visited) {
+            dfSearch(n);
+        }
+    });
 }
 
 function bfSearch(root) {
-  const queue = [];
-  root.visited = true;
-  queue.push(root);
-  while (queue.length) {
-    let removed = queue.shift();
-    console.log(removed.name);
-    removed.children.forEach(n => {
-      if (!n.visited) {
-        n.visited = true;
-        queue.push(n);
-      }
-    })
-  }
+    const queue = [];
+    root.visited = true;
+    queue.push(root);
+    while (queue.length) {
+        let removed = queue.shift();
+        console.log(removed.name);
+        removed.children.forEach((n) => {
+            if (!n.visited) {
+                n.visited = true;
+                queue.push(n);
+            }
+        });
+    }
 }
 
-let zero = new GraphNode(0);
-let one = new GraphNode(1);
-let two = new GraphNode(2);
-let three = new GraphNode(3);
-let four = new GraphNode(4);
-let five = new GraphNode(5);
+let [zero, one, two, three, four, five] = [
+    new GraphNode(0),
+    new GraphNode(1),
+    new GraphNode(2),
+    new GraphNode(3),
+    new GraphNode(4),
+    new GraphNode(5),
+];
 
 zero.children.push(one, four, five);
 one.children.push(three, four);
@@ -53,9 +55,10 @@ two.children.push(one);
 
 // crappy drawing of the graph;
 // 0 -> 1 <- 2
-// | \    \
-// v  v    v
+// | \    \  ^
+// v  v    v |
 // 5   4 <- 3
 
-// dfSearch(zero);
+dfSearch(zero);
+[zero, one, two, three, four, five].forEach((node) => (node.visited = false));
 bfSearch(zero);
