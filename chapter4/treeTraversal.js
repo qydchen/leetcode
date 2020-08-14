9c7a1db616e8b31f794707200d72d56d09cca3ca
David Chen <qydchen@gmail.com>
2020-08-14 10:34:37 -0400

    clean tree traversal

--- chapter4/treeTraversal.js
+++ chapter4/treeTraversal.js
@@ -1,51 +1,53 @@
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.left = this.right = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
 }
 
 // In-order traversal
 function inOrder(node) {
-  if (node !== null) {
-    inOrder(node.left);
-    console.log(node.val)
-    inOrder(node.right);
-  }
+    if (node !== null) {
+        inOrder(node.left);
+        console.log(node.val);
+        inOrder(node.right);
+    }
 }
 
 // Pre-order traversal
 function preOrder(node) {
-  if (node !== null) {
-    console.log(node.val)
-    preOrder(node.left);
-    preOrder(node.right);
-  }
+    if (node !== null) {
+        console.log(node.val);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
 }
 
 // Post-order traversal
 function postOrder(node) {
-  if (node !== null) {
-    postOrder(node.left);
-    postOrder(node.right);
-    console.log(node.val)
-  }
+    if (node !== null) {
+        postOrder(node.left);
+        postOrder(node.right);
+        console.log(node.val);
+    }
 }
 //       a
 //     b   c
 //   d  e  f  g
 
-let a = new Node('a');
-a.left = new Node('b');
-a.right = new Node('c');
-a.left.left = new Node('d');
-a.left.right = new Node('e');
-a.right.left = new Node('f');
-a.right.right = new Node('g');
+let a = new Node("a");
+a.left = new Node("b");
+a.right = new Node("c");
 
-console.log('in order');
+a.left.left = new Node("d");
+a.left.right = new Node("e");
+
+a.right.left = new Node("f");
+a.right.right = new Node("g");
+
+console.log("in order");
 inOrder(a);
-console.log('pre order');
+console.log("pre order");
 preOrder(a);
-console.log('post order');
+console.log("post order");
 postOrder(a);
