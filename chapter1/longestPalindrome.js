77e2fcf15f307f00bafa57626d4b00b7a338c1af
David Chen <qydchen@gmail.com>
2020-08-14 21:29:24 -0400

    submit longestPalindrome

---
+++ chapter1/longestPalindrome.js
@@ -0,0 +1,21 @@
+/**
+ * Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+ * This is case sensitive, for example "Aa" is not considered a palindrome here.
+ * Note:
+ * Assume the length of given string will not exceed 1, 010.
+ */
+
+function longestPalindrome(s) {
+    const set = new Set();
+    let counter = 0;
+    for (const str of s) {
+        if (set.has(str)) {
+            set.delete(str);
+            counter += 2;
+        } else {
+            set.add(str);
+        }
+    }
+    if (set.size > 0) counter += 1;
+    return counter;
+}
