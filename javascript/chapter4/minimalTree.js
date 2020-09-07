3cc0137a2bb3cd9fa7157933cbd658d255d8e80c
David Chen <qydchen@gmail.com>
2020-09-07 00:02:39 -0400

    reroganize"

--- javascript/chapter4/minimalTree.js
+++ javascript/chapter4/minimalTree.js
@@ -2,28 +2,28 @@
 // algorithm to create a binary search tree with minimal height
 
 class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = this.right = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
 }
 
 // O(n) time // time taken for an array of size n
 // O(h) space, where h is the height of the tree ????
 function minimalTree(array) {
-  if (array.length === 0) return null;
-  if (array.length === 1) return new TreeNode(array[0]);
-  const mid = Math.floor(array.length / 2);
-  const left = array.slice(0, mid);
-  const right = array.slice(mid + 1);
-  const node = new TreeNode(array[mid]);
-  node.left = minimalTree(left);
-  node.right = minimalTree(right);
-  return node;
+    if (array.length === 0) return null;
+    if (array.length === 1) return new TreeNode(array[0]);
+    const mid = Math.floor(array.length / 2);
+    const left = array.slice(0, mid);
+    const right = array.slice(mid + 1);
+    const node = new TreeNode(array[mid]);
+    node.left = minimalTree(left);
+    node.right = minimalTree(right);
+    return node;
 }
 
 const arr1 = [1, 2, 3, 5, 6, 7, 8];
-const arr2 = [1, 2, 3, 4, 5, 6, 7, 8];
+const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 8];
 const baseCase = [5];
 const evenCase = [4, 6];
 const oddCase = [1, 2, 3];
@@ -46,32 +46,32 @@ const ans5 = minimalTree(oddCase); // =>
 // 1   3
 
 function inOrderPrint(node) {
-  if (node) {
-    inOrderPrint(node.left);
-    console.log(node.val);
-    inOrderPrint(node.right);
-  }
+    if (node) {
+        inOrderPrint(node.left);
+        console.log(node.val);
+        inOrderPrint(node.right);
+    }
 }
 
 function preOrderPrint(node) {
-  if (node) {
-    console.log(node.val);
-    preOrderPrint(node.left);
-    preOrderPrint(node.right);
-  }
+    if (node) {
+        console.log(node.val);
+        preOrderPrint(node.left);
+        preOrderPrint(node.right);
+    }
 }
 
 // inOrderPrint(ans1)
-// inOrderPrint(ans2)
+// inOrderPrint(ans2);
 // inOrderPrint(ans3)
 // inOrderPrint(ans4)
 // inOrderPrint(ans5)
 
 module.exports = {
-  ans1,
-  ans2,
-  ans3,
-  ans4,
-  ans5,
-  TreeNode,
-}
+    ans1,
+    ans2,
+    ans3,
+    ans4,
+    ans5,
+    TreeNode,
+};
--- javascript/chapter4/validateBST.js
+++ javascript/chapter4/validateBST.js
@@ -1,19 +1,22 @@
 // Implement a function to check if a binary tree is a binary search tree.
-const { ans1, ans2, ans3, ans4, ans5, TreeNode } = require('./minimalTree');
+const { ans1, ans2, ans3, ans4, ans5, TreeNode } = require("./minimalTree");
+const assert = require("assert");
 // We can in-order traversal and check the prior node against the current node (assuming unique numbers)
 
 // We can pass the min and max values on each recursive call
 
 function validateBST(node) {
-  let lastPrinted = null; // encapsulate a variable outside of IIFE to track the lastPrinted element
-  return (function check(node) { // use a named IIFE to make the recursive call
-    if (node === null) return true;
-    if (!check(node.left)) return false; // check / recurse left
-    if (lastPrinted !== null && node.val <= lastPrinted) return false;
-    lastPrinted = node.val;
-    if (!check(node.right)) return false; // check / recurse right
-    return true;
-  })(node)
+    let lastPrinted = null; // encapsulate a variable outside of IIFE to track the lastPrinted element
+    return (function check(node) {
+        // use a named IIFE to make the recursive call
+        if (node === null) return true;
+        if (!check(node.left)) return false; // check / recurse left
+        if (lastPrinted !== null && node.val < lastPrinted) return false;
+        lastPrinted = node.val;
+        if (!check(node.right)) return false; // check / recurse right
+        console.log(node.val);
+        return true;
+    })(node);
 }
 
 let unbal = new TreeNode(5);
@@ -21,7 +24,15 @@ unbal.left = new TreeNode(3);
 unbal.right = new TreeNode(10);
 unbal.left.left = new TreeNode(1);
 unbal.left.right = new TreeNode(42);
+unbal.left.right.right = new TreeNode(42);
 
-console.log(validateBST(ans1)); // true
-console.log(validateBST(ans2)); // true
-console.log(validateBST(unbal)); // false
+// console.log(validateBST(ans1)); // true
+// console.log(validateBST(ans2)); // true
+// console.log(validateBST(unbal)); // false
+
+let test1 = new TreeNode(10);
+test1.left = new TreeNode(5);
+test1.right = new TreeNode(15);
+test1.left.right = new TreeNode(10);
+// solution doesnt work for this case
+assert.equal(validateBST(test1), false);
