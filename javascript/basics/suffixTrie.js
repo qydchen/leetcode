2eaa2d8769a29e80f9e3f9d0810f55d539ccb56f
David Chen <qydchen@gmail.com>
2020-09-15 00:23:57 -0400

    add complexity analysis

--- javascript/basics/suffixTrie.js
+++ javascript/basics/suffixTrie.js
@@ -5,6 +5,8 @@ class SuffixTrie {
         this.populateSuffixTrieFrom(string);
     }
 
+    // O(n^2) time where n is the length of the constructor string
+    // O(n^2) space
     populateSuffixTrieFrom(string) {
         for (let i = 0; i < string.length; i += 1) {
             let node = this.root;
@@ -19,6 +21,8 @@ class SuffixTrie {
         }
     }
 
+    // O(m) time where m is the input string
+    // O(1) space
     contains(string) {
         let search = this.root;
         for (let i = 0; i < string.length; i += 1) {
