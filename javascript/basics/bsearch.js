0a1b3e661902f3827849e702efa425019a6e7f42
David Chen <qydchen@gmail.com>
2020-09-19 13:56:07 -0400

    add solution

--- javascript/basics/bsearch.js
+++ javascript/basics/bsearch.js
@@ -15,3 +15,21 @@ function bsearch(array, target) {
 
 console.log(bsearch([2, 4, 6, 7, 8, 10, 13, 15, 17, 19, 20], 19)); // => 9
 console.log(bsearch([2, 4, 6, 7, 8, 10, 13, 15, 17, 19, 20], 18)); // => -1
+
+function bsearch2(array, target, leftIdx = 0, rightIdx = array.length - 1) {
+    let mid = Math.floor((leftIdx + rightIdx) / 2);
+    if (array[mid] === target) {
+        return mid;
+    } else if (array[mid] > target) {
+        return bsearch(array, target, leftIdx, mid);
+    } else if (array[mid] < target) {
+        return bsearch(array, target, mid + 1, rightIdx);
+    }
+    return -1;
+}
+
+console.log(bsearch2([2, 4, 6, 7, 8, 10, 13, 15, 17, 19, 20], 19)); // => 9
+console.log(bsearch2([2, 4, 6, 7, 8, 10, 13, 15, 17, 19, 20], 18)); // => -1
+console.log(bsearch2([1, 3, 4, 5, 6, 8, 9], 3)); // => 1
+console.log(bsearch2([], 0)); // => -1
+console.log(bsearch2([1, 3, 7, 8, 88, 99, 100], 100)); // => 6
