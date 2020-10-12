d10f95591ab300fbd1ec4f8e5ce7834ef598f0a2
David Chen <qydchen@gmail.com>
2020-10-11 22:16:50 -0400

    comment important concept

--- javascript/feelin-googly/mediums/wordSearch.js
+++ javascript/feelin-googly/mediums/wordSearch.js
@@ -1,3 +1,4 @@
+// **Important Concept**
 // Given a 2D board and a word, find if the word exists in the grid.
 
 // The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
@@ -28,59 +29,58 @@
  * @return {boolean}
  */
 
-// My solution... it fails some test cases though
 var exist = function (board, word) {
-  if (word.length > board.reduce((a, b) => a + b.length, 0)) return false;
-  const beginChar = word[0];
-  const beginningCoords = findBeginningCoords(beginChar, board);
-  for (let coord of beginningCoords) {
-    const visited = new Array(board.length)
-      .fill()
-      .map((_, i) => new Array(board[i].length).fill(false));
-    if (traverse(coord, word, board, visited)) {
-      return true;
+    if (word.length > board.reduce((a, b) => a + b.length, 0)) return false;
+    const beginChar = word[0];
+    const beginningCoords = findBeginningCoords(beginChar, board);
+    for (let coord of beginningCoords) {
+        const visited = new Array(board.length)
+            .fill()
+            .map((_, i) => new Array(board[i].length).fill(false));
+        if (traverse(coord, word, board, visited)) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 };
 
 function traverse(coord, word, board, visited) {
-  const [i, j] = coord;
-  const width = board[0].length - 1;
-  const height = board.length - 1;
-  if (word.length === 0) return true;
-  if (j < 0 || i < 0 || j > width || i > height) return false;
-  if (board[i][j] === word[0] && visited[i][j] === false) {
-    visited[i][j] = true;
-    if (
-      traverse([i, j + 1], word.slice(1), board, visited) ||
-      traverse([i, j - 1], word.slice(1), board, visited) ||
-      traverse([i - 1, j], word.slice(1), board, visited) ||
-      traverse([i + 1, j], word.slice(1), board, visited)
-    ) {
-      return true;
+    const [i, j] = coord;
+    const width = board[0].length - 1;
+    const height = board.length - 1;
+    if (word.length === 0) return true;
+    if (j < 0 || i < 0 || j > width || i > height) return false;
+    if (board[i][j] === word[0] && visited[i][j] === false) {
+        visited[i][j] = true;
+        if (
+            traverse([i, j + 1], word.slice(1), board, visited) ||
+            traverse([i, j - 1], word.slice(1), board, visited) ||
+            traverse([i - 1, j], word.slice(1), board, visited) ||
+            traverse([i + 1, j], word.slice(1), board, visited)
+        ) {
+            return true;
+        }
+        visited[i][j] = false;
     }
-    visited[i][j] = false;
-  }
-  return false;
+    return false;
 }
 
 function findBeginningCoords(char, board) {
-  let result = [];
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (board[i][j] === char) {
-        result.push([i, j]);
-      }
+    let result = [];
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] === char) {
+                result.push([i, j]);
+            }
+        }
     }
-  }
-  return result;
+    return result;
 }
 
 let b = [
-  ["A", "B", "C", "E"],
-  ["S", "F", "C", "S"],
-  ["A", "D", "E", "E"],
+    ["A", "B", "C", "E"],
+    ["S", "F", "C", "S"],
+    ["A", "D", "E", "E"],
 ];
 
 console.log(exist(b, "ABCCED")); //true
@@ -92,12 +92,12 @@ console.log(exist(c, "A".repeat(12))); // true
 console.log(exist(c, "A".repeat(13))); // false
 
 let d = [
-  ["a", "b", "b", "a", "b"],
-  ["a", "a", "b", "b", "a"],
-  ["a", "a", "a", "a", "b"],
-  ["a", "a", "a", "b", "a"],
-  ["a", "a", "a", "a", "a"],
-  ["a", "b", "a", "b", "b"],
-  ["a", "b", "b", "a", "b"],
+    ["a", "b", "b", "a", "b"],
+    ["a", "a", "b", "b", "a"],
+    ["a", "a", "a", "a", "b"],
+    ["a", "a", "a", "b", "a"],
+    ["a", "a", "a", "a", "a"],
+    ["a", "b", "a", "b", "b"],
+    ["a", "b", "b", "a", "b"],
 ];
 console.log(exist(d, "abbbbaababaa")); // false
