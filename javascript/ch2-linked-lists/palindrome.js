9f8ba6f7253fd7903df05f676991a9887c6b6ddb
qydchen <qydchen@gmail.com>
2021-01-03 15:57:53 -0500

    refactor and clean linked list palindrome

--- javascript/ch2-linked-lists/palindrome.js
+++ javascript/ch2-linked-lists/palindrome.js
@@ -18,7 +18,8 @@ class ListNode {
 
 function Stack() {
   let stack = []; // make the stack a private variable
-  return (() => { // use IIFE to encapsulate
+  return (() => {
+    // use IIFE to encapsulate
     this.size = 0;
     this.pop = () => {
       if (stack.pop()) {
@@ -32,16 +33,11 @@ function Stack() {
       this.size++;
     };
     this.peek = () => stack[stack.length - 1];
-  })()
+  })();
 }
 
 function checkPalindrome(node) {
   let stack = new Stack();
-  if (node !== null) {
-    stack.push(node);
-  } else {
-    return true;
-  }
   let current = node;
   let fast = node;
   while (fast !== null && fast.next !== null) {
@@ -49,7 +45,8 @@ function checkPalindrome(node) {
     fast = fast.next.next;
     current = current.next;
   }
-  if (fast !== null) { // handles odd # of nodes
+  if (fast !== null) {
+    // handles odd # of nodes
     current = current.next;
   }
   while (current !== null) {
@@ -62,21 +59,20 @@ function checkPalindrome(node) {
   return true;
 }
 
-
-let head = new ListNode('a');
-head.next = new ListNode('b');
-head.next.next = new ListNode('a');
+let head = new ListNode("a");
+head.next = new ListNode("b");
+head.next.next = new ListNode("a");
 
 console.log(checkPalindrome(head)); // => true;
 // head.print(head);
 
-let head2 = new ListNode('r');
-head2.next = new ListNode('a');
-head2.next.next = new ListNode('c');
-head2.next.next.next = new ListNode('e');
-head2.next.next.next.next = new ListNode('c');
-head2.next.next.next.next.next = new ListNode('a');
-head2.next.next.next.next.next.next = new ListNode('r');
+let head2 = new ListNode("r");
+head2.next = new ListNode("a");
+head2.next.next = new ListNode("c");
+head2.next.next.next = new ListNode("e");
+head2.next.next.next.next = new ListNode("c");
+head2.next.next.next.next.next = new ListNode("a");
+head2.next.next.next.next.next.next = new ListNode("r");
 
 console.log(checkPalindrome(head2)); // => true;
 // head2.print(head2);
@@ -84,28 +80,33 @@ console.log(checkPalindrome(head2)); // => true;
 let head3 = null;
 console.log(checkPalindrome(head3)); // => true;
 
-let head4 = new ListNode('a');
+let head4 = new ListNode("a");
 console.log(checkPalindrome(head4)); // => true;
 
-let head5 = new ListNode('a');
-head5.next = new ListNode('a');
+let head5 = new ListNode("a");
+head5.next = new ListNode("a");
 console.log(checkPalindrome(head5)); // => true;
 
-let head6 = new ListNode('a');
-head6.next = new ListNode('b');
+let head6 = new ListNode("a");
+head6.next = new ListNode("b");
 console.log(checkPalindrome(head6)); // => false;
 
-let head7 = new ListNode('a');
-head7.next = new ListNode('b');
-head7.next.next = new ListNode('b');
+let head7 = new ListNode("a");
+head7.next = new ListNode("b");
+head7.next.next = new ListNode("b");
 console.log(checkPalindrome(head7)); // => false;
 
-let head8 = new ListNode('r');
-head8.next = new ListNode('a');
-head8.next.next = new ListNode('x');
-head8.next.next.next = new ListNode('e');
-head8.next.next.next.next = new ListNode('c');
-head8.next.next.next.next.next = new ListNode('a');
-head8.next.next.next.next.next.next = new ListNode('r');
+let head8 = new ListNode("r");
+head8.next = new ListNode("a");
+head8.next.next = new ListNode("x");
+head8.next.next.next = new ListNode("e");
+head8.next.next.next.next = new ListNode("c");
+head8.next.next.next.next.next = new ListNode("a");
+head8.next.next.next.next.next.next = new ListNode("r");
 
 console.log(checkPalindrome(head8)); // => false;
+
+let head9 = new ListNode(1);
+head9.next = new ListNode(0);
+
+console.log(checkPalindrome(head9));
