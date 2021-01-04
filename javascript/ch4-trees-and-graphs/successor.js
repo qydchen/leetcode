9d41c25762071c679ebee71a4f3273149c55dd33
qydchen <qydchen@gmail.com>
2021-01-03 22:09:36 -0500

    delete shit heap

--- javascript/ch4-trees-and-graphs/successor.js
+++
@@ -1,9 +0,0 @@
-// Write an algorithm to find the 'next' node (i.e., in-order successor) of
-// a given node in a binary search tree. You may assume that each node has
-// a link to its parent.
-
-// REVIEW
-const { ans1, ans2, ans3, ans4, ans5 } = require('./minimalTree');
-function successor(node) {
-
-}
