3365ad54b7715e4bffcd228bd65440301d60b784
qydchen <qydchen@gmail.com>
2021-01-12 23:37:22 -0500

    sum even grandparent

---
+++ javascript/ch4-trees-and-graphs/sumEvenGrandparent.js
@@ -0,0 +1,30 @@
+// Given a binary tree, return the sum of values of nodes with even-valued grandparent.  (A grandparent of a node is the parent of its parent, if it exists.)
+
+// If there are no nodes with an even-valued grandparent, return 0.
+
+// Example 1:
+
+// Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+// Output: 18
+// Explanation: The red nodes are the nodes with even-value grandparent while the blue nodes are the even-value grandparents.
+
+// Constraints:
+
+// The number of nodes in the tree is between 1 and 10^4.
+// The value of nodes is between 1 and 100.
+
+var sumEvenGrandparent = function (root) {
+  let sum = 0;
+  dfs(root, null, null);
+  return sum;
+  function dfs(node, parent, grandparent) {
+    if (!node) return 0;
+    if (grandparent && grandparent.val % 2 === 0) {
+      sum += node.val;
+    } else {
+      sum += 0;
+    }
+    dfs(node.left, node, parent);
+    dfs(node.right, node, parent);
+  }
+};
