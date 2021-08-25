18171a5ea7ce600e0697c2a5b646fe145739cf92
qydchen <qydchen@gmail.com>
2021-08-24 20:46:55 -0400

    notes for batch-9 lecture-5

--- javascript/hicounselor/batch8/lecture1.js
+++ javascript/hicounselor/batch8/lecture1.js
@@ -1,6 +1,6 @@
 // Given a number and power, return the exponent
 
-// exponent(base, power) 
+// exponent(base, power)
 // exponent(2, 4) // => 16
 
 // BASE CASE
@@ -19,17 +19,16 @@
 // f(n) = base * f(n - 1)
 
 function exponent(base, pow) {
-    // base case
-    if (pow === 0) {
-        return 1;
-    }
-    // iterative step
-    return base * exponent(base, pow - 1);
+  // base case
+  if (pow === 0) {
+    return 1;
+  }
+  // iterative step
+  return base * exponent(base, pow - 1);
 }
 
 // console.log(exponent(2, 4));
 // console.log(exponent(3, 2));
-
 
 // [1,1,2,3,5,8,13,21]
 
@@ -49,12 +48,12 @@ function exponent(base, pow) {
 // f(4) => f(3) + f(2)
 // f(n) => f(n - 1) + f(n - 2)
 function fibs(n) {
-    // base case
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-    // recursive step
-    return fibs(n - 1) + fibs(n - 2);
+  // base case
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  // recursive step
+  return fibs(n - 1) + fibs(n - 2);
 }
 
 // console.log(fibs(2)) // 2
@@ -80,17 +79,17 @@ function fibs(n) {
 // space complexity O(n) where n is the input
 
 function fibsMemo(n, memo = {}) {
-    if (memo[n]) {
-        return memo[n];
-    }
-    // base case
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-    // recursive step
-    const result = fibs(n - 1, memo) + fibs(n - 2, memo);
-    memo[n] = result;
+  if (memo[n]) {
     return memo[n];
+  }
+  // base case
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  // recursive step
+  const result = fibs(n - 1, memo) + fibs(n - 2, memo);
+  memo[n] = result;
+  return memo[n];
 }
 
 // time complexity => O(n) // memoization has reduced the repetitive calls, and thus the
@@ -101,16 +100,15 @@ function fibsMemo(n, memo = {}) {
 // console.log(fibs(3)) // 3
 // console.log(fibs(40));
 
-
 // Given a map where keys are employeeIds and values are a list of employeeIds, given an employeeIds as the argument
 // Tell me how many people are in the employeeId's reporting line
 
 const reportingLine = {
-    a: ['b','c,','d'],
-    b: ['e','f','g'],
-    g: ['x','y'],
-    y: ['z']
-}
+  a: ["b", "c,", "d"],
+  b: ["e", "f", "g"],
+  g: ["x", "y"],
+  y: ["z"],
+};
 //           a
 //        /  |  \
 //       b   c   d
@@ -122,17 +120,17 @@ const reportingLine = {
 //         z
 
 function countSubordinatesFromReportingLine(hierarchy, employeeIdTarget) {
-    let queue = []; 
-    queue.push(employeeIdTarget); // ['b']
-    let count = -1;
-    while (queue.length > 0) {
-        let current = queue.shift(); // [] // current = 'b'
-        count++;
-        if (hierarchy[current] !== undefined) {
-            queue = queue.concat(hierarchy[current]); // [e,f,g]
-        }
+  let queue = [];
+  queue.push(employeeIdTarget); // ['b']
+  let count = -1;
+  while (queue.length > 0) {
+    let current = queue.shift(); // [] // current = 'b'
+    count++;
+    if (hierarchy[current] !== undefined) {
+      queue = queue.concat(hierarchy[current]); // [e,f,g]
     }
-    return count;
+  }
+  return count;
 }
 // time complexity = O(n) // where n is the entire population of the hierarchy
 // space complexity = O(n) // where worse case is that there is one employee with all n - 1 people in the hierarchy
@@ -145,7 +143,7 @@ function countSubordinatesFromReportingLine(hierarchy, employeeIdTarget) {
 // Given a collection of coin denominations, and a target cent value, find the minimum
 // number of possible coins that can be used to make that value
 
-const denoms = [1,5,10,25];
+const denoms = [1, 5, 10, 25];
 // const target = 42;
 // 1 quarter, 1 nickel, 1 dime, 2 pennies // 5
 
@@ -158,7 +156,7 @@ const target = 6;
 // tab = [0, 1, 2, 3, 4, 5, 6]; where 1
 // we are in the second denom 5
 // tab = [0, 0, 0, 0, 0, 1, (6 - 5) + 1]; where denom is 5
-                                    //^ nickel
+//^ nickel
 // [1, 5, 10, 25]
 
 // current tabulation = 1
@@ -177,12 +175,3 @@ function minNumberOfCoinsForChange(n, denoms) {
   }
   return mins[mins.length - 1] !== Infinity ? mins[mins.length - 1] : -1;
 }
-
-
-
-
-
-
-
-
-
