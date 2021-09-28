f7ab773792b7c7ae48a6a8d580ba5d61c2746531
qydchen <qydchen@gmail.com>
2021-09-28 17:27:48 -0400

    prep lecture 3'

---
+++ javascript/hicounselor/batch10/lecture3.js
@@ -0,0 +1,38 @@
+// topological sort
+
+// islands
+
+const grid1 = [
+  [1, 1, 1, 1, 0],
+  [1, 1, 0, 1, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+]; //1
+
+const grid2 = [
+  [1, 1, 0, 0, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 0, 1, 1],
+]; // 3
+
+const grid3 = [
+  [1, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 1, 1],
+  [0, 0, 1, 0, 1, 0],
+  [1, 1, 0, 0, 1, 0],
+  [1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 0, 1],
+]; // 7
+
+// Given a 2d grid map of 1s (land) and 0s (water), count the number of islands.
+// An island is surrounded by water and is formed by connecting adjacent lands horizontally
+// or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+const numberOfIslands = (grid) => {};
+
+// Given a matrix, remove all islands in the matrix that does not touch the border of the matrix
+/*
+ */
+
+const removeIslands = (matrix) => {};
