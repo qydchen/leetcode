be8f82df8d92c0fc6e38dad8c165e9fed95899a3
qydchen <qydchen@gmail.com>
2021-11-04 21:20:14 -0400

    submit session 2 for batch 13 git push

--- javascript/hicounselor/batch13/lecture2.js
+++ javascript/hicounselor/batch13/lecture2.js
@@ -1,8 +1,9 @@
 // Trees
 
 // define node & edge
+// A node is a data structure of a tree that stores data
+// An edge is a reference between two nodes
 
-// in order
 class TreeNode {
   constructor(val) {
     this.val = val;
@@ -27,6 +28,105 @@ class TreeNode {
 // root.right.right.left.right = new TreeNode(10);
 // root.right.right.right.right = new TreeNode(42);
 
+function inOrderTraversal(node) {
+  if (node !== null) {
+    inOrderTraversal(node.left);
+    console.log(node.val);
+    inOrderTraversal(node.right);
+  }
+}
+
+// inOrderTraversal(root); // expect to print 1 2 3 4
+// time complexity O(n)
+// space complexity O(n)
+
+// pre order
+function preOrderTraversal(node) {
+  if (node !== null) {
+    console.log(node.val);
+    preOrderTraversal(node.left);
+    preOrderTraversal(node.right);
+  }
+}
+
+//    3
+//   /  \
+//  2   4
+// /
+// 1
+
+// preOrderTraversal(root); // 3 2 1 4
+
+// post order
+function postOrderTraversal(node) {
+  if (node !== null) {
+    postOrderTraversal(node.left);
+    postOrderTraversal(node.right);
+    console.log(node.val);
+  }
+}
+
+// postOrderTraversal(root); // 1 2 4 3
+
+// time / space complexity
+
+// time O(n) where n is the number of nodes in the binary tree
+// space O(n) where n is the number of nodes in the binary tree if the binary tree is reduced to a linked list
+
+// dfs, bfs
+function dfs(node, searchVal) {
+  if (node == null) {
+    return;
+  }
+  if (node.val === searchVal) {
+    return node;
+  }
+  let left = dfs(node.left, searchVal);
+  let right = dfs(node.right, searchVal);
+  if (left != null) {
+    return left;
+  }
+  if (right != null) {
+    return right;
+  }
+  return null;
+}
+
+// console.log(dfs(root, 55));
+// console.log(dfs(root, 1));
+// console.log(dfs(root, -99));
+// console.log(dfs(root, 0));
+
+function bfs(node, searchVal) {
+  if (node == null) return;
+  let queue = [];
+  // enqueue the current root node;
+  queue.push(node);
+  while (queue.length !== 0) {
+    let curr = queue.shift();
+    if (curr.val === searchVal) {
+      return curr;
+    }
+    if (curr.left !== null) {
+      queue.push(curr.left);
+    }
+    if (curr.right !== null) {
+      queue.push(curr.right);
+    }
+  }
+  return null;
+}
+
+// console.log(bfs(root, 55));
+// console.log(bfs(root, 1));
+// console.log(bfs(root, -99));
+// console.log(bfs(root, 0));
+
+// getMaxTree
+
+// Given a binary tree of all integers, return an array of integers where each index of the array represents the 'level' of a tree, and the value in the array
+// is the maximum value in that level
+
 //    3
 //   /  \
 //  2   4
@@ -37,47 +137,53 @@ class TreeNode {
 //    / \   \
 //   9  10  42
 
-function inOrderTraversal(node) {}
-
-// inOrderTraversal(root); // expect to print 1 2 3 4
-
-// pre order
-function preOrderTraversal(node) {}
-
-// preOrderTraversal(root); // 3 2 1 4
-
-// post order
-function postOrderTraversal(node) {}
-
-// postOrderTraversal(root); // 1 2 4 3
-
-// time / space complexity
-
-// time O(n) where n is the number of nodes in the binary tree
-// space O(n) where n is the number of nodes in the binary tree if the binary tree is reduced to a linked list
-
-// dfs, bfs
-function dfs(node, searchVal) {}
-
-function bfs(node, searchVal) {}
-
-// getMaxTree
-
-// Given a binary tree of all integers, return an array of integers where each index of the array represents the 'level' of a tree, and the value in the array
-// is the maximum value in that level
-
-function getMaxTree(node) {}
+function getMaxTree(node) {
+  if (node == null) {
+    return [];
+  }
+  let maxes = [];
+  let queue = [];
+  queue.push(node);
+  while (queue.length !== 0) {
+    let childQueue = [];
+    let max = Math.max(...queue.map((n) => n.val));
+    maxes.push(max);
+    for (let n of queue) {
+      if (n.left !== null) {
+        childQueue.push(n.left);
+      }
+      if (n.right !== null) {
+        childQueue.push(n.right);
+      }
+    }
+    queue = childQueue;
+  }
+  return maxes;
+}
 
 // console.log(getMaxTree(root)); // [3,4,66,55,42]
-// Time O(n) where n is the numbers of nodes in the tree
-// Space O(h) where h is the height of the tree
 
 // bottom up solution
+
+//    3
+//   /  \
+//  2   4
+// /     \
+// 1     66
+//      / \
+//     55  11
+//    / \   \
+//   9  10  42
 
 // getHeight
 // Given a binary tree, return the height of the binary tree
 
-function getHeight(root) {}
+function getHeight(root) {
+  if (root === null) return 0;
+  return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+}
+
+// 1 + Math.max(getHeight(root.left), getHeight(root.right))
 
 // console.log(getHeight(root)); // 5
 
@@ -88,7 +194,7 @@ function getHeight(root) {}
 // directed graph
 // A graph where edges have direction
 
-// 2 ways to represent graphs
+// // 2 ways to represent graphs
 class GraphNode {
   constructor(val) {
     this.val = val;
@@ -101,7 +207,7 @@ const adjacencyList = {
   a: ["b", "c,", "d"],
   b: ["e", "f", "g"],
   g: ["x", "y"],
-  y: ["z"],
+  y: ["z", "a"],
 };
 
 let [zero, one, two, three, four, five] = [
@@ -122,15 +228,45 @@ two.children.push(one);
 // 0 -> 1 <- 2
 // | \    \  ^
 // v  v    v |
-// 5   4 <- 3
+// 5   4  <- 3
 
-function dfSearch(node, target) {}
+function dfSearch(node, target) {
+  if (node === null || node.visited === true) return null;
+  if (node.val === target) return node;
+  node.visited = true;
+  for (let child of node.children) {
+    let searched = dfSearch(child, target);
+    if (searched !== null) {
+      return searched;
+    }
+  }
+  return null;
+}
 
 // console.log(dfSearch(zero, 2));
+// console.log(dfSearch(zero, 55));
+// console.log(dfSearch(zero, 4));
 
-function bfSearch(node, target) {}
+function bfSearch(node, target) {
+  if (node === null) return null;
+  let queue = [];
+  queue.push(node);
+  while (queue.length !== 0) {
+    let curr = queue.shift();
+    if (curr.val === target) return curr;
+    curr.visited = true;
+    for (let n of curr.children) {
+      if (n.visited === false) {
+        queue.push(n);
+      }
+    }
+  }
+  return null;
+}
 
-// Time Complexity O(n + m) where n is the number of nodes and m is the number of edges
-// Space Complexity O(n + m) where n is the number of nodes and m is the number of edges
+// // Time Complexity O(n + m) where n is the number of nodes and m is the number of edges
+// // Space Complexity O(n + m) where n is the number of nodes and m is the number of edges
 
 // console.log(bfSearch(zero, 2));
+// console.log(bfSearch(zero, 55));
+console.log(bfSearch(zero, 4));
