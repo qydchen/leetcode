88f1d42b0d3d7a4a00acc4ad34370b1c72a4a068
David Chen <qydchen@gmail.com>
2022-01-17 20:05:48 -0500

    lecture 3 b16

---
+++ javascript/hicounselor/batch16/lecture3.js
@@ -0,0 +1,211 @@
+// Graphs
+// undirected graph
+// A graph where edges have no direction
+
+//     "a"-"b"
+
+// directed graph
+// A graph where edges have direction
+
+//     "a"->"b"
+
+// 2 ways to represent graphs
+class GraphNode {
+  constructor(val) {
+    this.val = val;
+    this.children = [];
+    this.visited = false;
+  }
+}
+
+const adjacencyList = {
+  a: ["b", "c,", "d"],
+  b: ["e", "f", "g"],
+  g: ["x", "y"],
+  y: ["z"],
+};
+
+let [zero, one, two, three, four, five] = [
+  new GraphNode(0),
+  new GraphNode(1),
+  new GraphNode(2),
+  new GraphNode(3),
+  new GraphNode(4),
+  new GraphNode(5),
+];
+
+zero.children.push(one, four, five);
+one.children.push(three, four);
+three.children.push(two, four);
+two.children.push(one);
+
+// crappy drawing of the graph;
+// 0 -> 1 <- 2
+// | \    \  ^
+// v  v    v |
+// 5   4 <- 3
+
+function dfSearch(node, target) {
+  if (node == null) return null;
+  if (node.visited === true) return null;
+  node.visited = true;
+  if (node.val === target) return node;
+  for (let child of node.children) {
+    let search = dfSearch(child, target);
+    if (search != null) {
+      return search;
+    }
+  }
+  return null;
+}
+
+// console.log(dfSearch(zero, 2));
+
+function bfSearch(node, target) {
+  if (node === null) return null;
+  let queue = [];
+  queue.push(node);
+  while (queue.length > 0) {
+    let curr = queue.shift();
+    curr.visited = true;
+    if (curr.val === target) return curr;
+    for (let child of curr.children) {
+      if (child.visited === false) {
+        queue.push(child);
+      }
+    }
+  }
+  return null;
+}
+
+// topological sort
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.children = [];
+  }
+}
+
+//             Class C
+//         /           \
+// Class A                 Class J - Class K
+//         \           /           /
+//             Class D            /
+//                               /
+// Class B ---------------------
+
+const input = [
+  ["ClassD", "ClassJ"],
+  ["ClassA", "ClassC"],
+  ["ClassA", "ClassD"],
+  ["ClassB", "ClassK"],
+  ["ClassJ", "ClassK"],
+  ["ClassC", "ClassJ"],
+];
+
+const input2 = [
+  [1, 2],
+  [1, 3],
+  [2, 4],
+  [3, 4],
+];
+
+// 1 - 2 - 4
+//   \ 3 /
+
+const topologicalSort = (edges) => {};
+
+// console.log(topologicalSort(input));
+// console.log(topologicalSort(input2));
+// console.log(
+//   topologicalSort([
+//     ["a", "b"],
+//     ["b", "c"],
+//     ["c", "a"],
+//   ])
+// );
+
+// islands
+const grid1 = [
+  [1, 1, 1, 1, 0],
+  [1, 1, 0, 1, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+]; // 1
+
+const grid2 = [
+  [1, 1, 0, 0, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 0, 1, 1],
+]; // 3
+
+const grid3 = [
+  [1, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 1, 1],
+  [0, 0, 1, 0, 1, 0],
+  [1, 1, 0, 0, 1, 0],
+  [1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 0, 1],
+]; // 7
+
+// Given a 2d grid map of 1s (land) and 0s (water), count the number of islands.
+// An island is surrounded by water and is formed by connecting adjacent lands horizontally
+// or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+const numberOfIslands = (grid) => {};
+
+// console.log(numberOfIslands(grid1));
+// console.log(numberOfIslands(grid2));
+// console.log(numberOfIslands(grid3));
+
+// Given a grid, remove all islands in the grid that does not touch the border of the grid
+const removeIslands = (grid) => {};
+
+const input0 = [
+  [1, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 1, 1],
+  [0, 0, 1, 0, 1, 0],
+  [1, 1, 0, 0, 1, 0],
+  [1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 0, 1],
+];
+
+console.log(removeIslands(input0));
+
+// Given a 2d grid map of 1s (land) and 0s (water), return the size of the biggest island
+
+// Given a 2d grid map of 1s (land) and 0s (water), return the median size of all the islands
+
+// Design an algorithm and write code to find the first common ancestor of two
+// nodes in a graph.
+
+class GraphNode {
+  constructor(val) {
+    this.val = val;
+    this.children = [];
+  }
+}
+
+const a = new GraphNode("a");
+const b = new GraphNode("b");
+const c = new GraphNode("c");
+const d = new GraphNode("d");
+const e = new GraphNode("e");
+const f = new GraphNode("f");
+const g = new GraphNode("g");
+const h = new GraphNode("h");
+const i = new GraphNode("i");
+const x = new GraphNode("x");
+
+a.children = [b, c, d];
+b.children = [e, f];
+c.children = [g, i];
+g.children = [h];
+h.children = [x];
+
+// console.log(LCA(a, h, i)); // c
+// console.log(LCA(a, e, f)); // b
+// console.log(LCA(a, a, a)); // a
+// console.log(LCA(a, g, h)); // g
