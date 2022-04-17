aeb59123fe5ffb8c6bb2b1fac748e59d6a02dfcb
David Chen <qydchen@gmail.com>
2022-04-17 19:05:33 -0400

    more tests

--- javascript/ch1-array-and-strings/generateStudyGroup.js
+++ javascript/ch1-array-and-strings/generateStudyGroup.js
@@ -21,6 +21,7 @@ function generateStudyGroup(scores) {
         group.max = Math.max(score, group.max);
         group.min = Math.min(score, group.min);
         group.collection.push(name);
+        break;
       }
     }
     if (diff === null || diff > 15) {
@@ -36,3 +37,19 @@ function generateStudyGroup(scores) {
 
 const input = { A: 20, B: 36, C: 40 };
 console.log(generateStudyGroup(input));
+const input2 = { A: 20, B: 22, C: 30 };
+console.log(generateStudyGroup(input2));
+const input3 = {
+  A: 20,
+  B: 22,
+  C: 30,
+  D: 35,
+  E: 36,
+  F: 50,
+  G: 70,
+  H: 10,
+  I: 299,
+  J: 0,
+  K: 5,
+};
+console.log(generateStudyGroup(input3));
