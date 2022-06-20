5e7860cf070ab8ca0c85290ae0d45a9adc74632b
David Chen <qydchen@gmail.com>
2022-06-20 12:53:55 -0400

    checkpoint

--- javascript/whiteboard.js
+++ javascript/whiteboard.js
@@ -1,46 +1,118 @@
-// Scratch pad
-var arrayLike = { 0: "oh", 1: "my", 2: "god", length: 3 };
-var arr = Array.prototype.map.bind(arrayLike)((el) => el);
-var arr = Array.prototype.reduce.bind(arrayLike)((acc, el) => acc + el);
-// var string = 'hi there!';
-// var arr = Array.prototype.map.bind(string)(el => el + 'x');
+class HashMap {
+  #storage = [];
+  #count = 0;
+  #capacity = 8;
+  insert(key, value) {
+    //create an index for our storage location by passing it through our hashing function
+    var index = this.hashFunc(key, this.#capacity);
+    //retrieve the bucket at this particular index in our storage, if one exists
+    //[[ [k,v], [k,v], [k,v] ] , [ [k,v], [k,v] ]  [ [k,v] ] ]
+    var bucket = this.#storage[index];
+    //does a bucket exist or do we get undefined when trying to retrieve said index?
+    if (!bucket) {
+      //create the bucket
+      var bucket = [];
+      //insert the bucket into our hashTable
+      this.#storage[index] = bucket;
+    }
 
-console.log(arr);
+    var override = false;
+    //now iterate through our bucket to see if there are any conflicting
+    //key value pairs within our bucket. If there are any, override them.
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      if (tuple[0] === key) {
+        //overide value stored at this key
+        tuple[1] = value;
+        override = true;
+      }
+    }
 
-function Person(name, age) {
-  this.getAge = function () {
-    console.log(age);
-  };
+    if (!override) {
+      //create a new tuple in our bucket
+      //note that this could either be the new empty bucket we created above
+      //or a bucket with other tupules with keys that are different than
+      //the key of the tuple we are inserting. These tupules are in the same
+      //bucket because their keys all equate to the same numeric index when
+      //passing through our hash function.
+      bucket.push([key, value]);
+      this.#count++;
+      //now that we've added our new key/val pair to our storage
+      //let's check to see if we need to resize our storage
+      if (this.#count > this.#capacity * 0.75) {
+        this.resize(this.#capacity * 2);
+      }
+    }
+    return this;
+  }
 
-  this.getName = function () {
-    console.log(name);
-  };
+  remove(key) {
+    var index = this.hashFunc(key, this.#capacity);
+    var bucket = this.#storage[index];
+    if (!bucket) {
+      return null;
+    }
+    //iterate over the bucket
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      //check to see if key is inside bucket
+      if (tuple[0] === key) {
+        //if it is, get rid of this tuple
+        bucket.splice(i, 1);
+        this.#count--;
+        if (this.#count < this.#capacity * 0.25) {
+          this._resize(this.#capacity / 2);
+        }
+        return tuple[1];
+      }
+    }
+  }
 
-  this.setAge = function (num) {
-    4;
-    age = num;
-    console.log(age);
-  };
+  retrieve(key) {
+    var index = this.hashFunc(key, this.#capacity);
+    var bucket = this.#storage[index];
 
-  this.setName = function (str) {
-    name = str;
-    console.log(name);
-  };
+    if (!bucket) {
+      return null;
+    }
+
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      if (tuple[0] === key) {
+        return tuple[1];
+      }
+    }
+
+    return null;
+  }
+
+  hashFunc(str, max) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+      var letter = str[i];
+      hash = (hash << 5) + letter.charCodeAt(0);
+      hash = (hash & hash) % max;
+    }
+    return hash;
+  }
+
+  resize(newLimit) {
+    var oldStorage = this.#storage;
+
+    this.#capacity = newLimit;
+    this.#count = 0;
+    this.#storage = [];
+
+    oldStorage.forEach(
+      function (bucket) {
+        if (!bucket) {
+          return;
+        }
+        for (var i = 0; i < bucket.length; i++) {
+          var tuple = bucket[i];
+          this.insert(tuple[0], tuple[1]);
+        }
+      }.bind(this)
+    );
+  }
 }
-
-// let Dave = new Person('Dave', 24)
-// Dave.getAge()
-// Dave.setAge(10)
-// Dave.getAge()
-//
-// Dave.getName()
-// Dave.setName('John')
-// Dave.getName()
-
-// let dude = new Person('guy', 24);
-// dude.getAge()
-// dude.getName()
-// dude.setAge(40)
-// dude.setName('bro')
-// dude.getAge()
-// dude.getName()
