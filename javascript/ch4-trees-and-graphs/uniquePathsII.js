afec32d064d116110672d03b0ecec7707889a40d
David Chen <qydchen@gmail.com>
2024-01-05 21:03:47 -0800

    LC 63 medium unique paths II

---
+++ javascript/ch4-trees-and-graphs/uniquePathsII.js
@@ -0,0 +1,58 @@
+// You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+// An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+
+// Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+// The testcases are generated so that the answer will be less than or equal to 2 * 109.
+
+// Example 1:
+
+// Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+// Output: 2
+// Explanation: There is one obstacle in the middle of the 3x3 grid above.
+// There are two ways to reach the bottom-right corner:
+// 1. Right -> Right -> Down -> Down
+// 2. Down -> Down -> Right -> Right
+// Example 2:
+
+// Input: obstacleGrid = [[0,1],[0,0]]
+// Output: 1
+
+// Constraints:
+
+// m == obstacleGrid.length
+// n == obstacleGrid[i].length
+// 1 <= m, n <= 100
+// obstacleGrid[i][j] is 0 or 1.
+
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+const serialize = (m, n) => `${m},${n}`;
+var uniquePathsWithObstacles = function (obstacleGrid) {
+  let memo = {};
+  const dfs = (m, n) => {
+    const serial = serialize(m, n);
+    if (serial in memo) {
+      return memo[serial];
+    }
+    if (m >= obstacleGrid.length || n >= obstacleGrid[0].length) {
+      return 0;
+    }
+    if (obstacleGrid[m][n] === 1) {
+      return 0;
+    }
+    if (m === obstacleGrid.length - 1 && n === obstacleGrid[0].length - 1) {
+      return 1;
+    }
+    let left = dfs(m, n + 1);
+    let right = dfs(m + 1, n);
+    let ans = left + right;
+    memo[serial] = ans;
+    return memo[serial];
+  };
+
+  return dfs(0, 0);
+};
