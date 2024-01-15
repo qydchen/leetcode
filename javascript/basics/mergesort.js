986b106dea5224d055af1b92d9324319171f6402
David Chen <qydchen@gmail.com>
2024-01-15 14:22:48 -0800

    update js merge sort

--- javascript/basics/mergesort.js
+++ javascript/basics/mergesort.js
@@ -1,28 +1,43 @@
 // Time: O(nlog(n)); Space O(nlog(n))
 
-function mergeSort(array) {
-  if (array.length <= 1) return array;
-  let mid = Math.floor(array.length / 2);
-  let left = mergeSort(array.slice(0, mid));
-  let right = mergeSort(array.slice(mid));
-  return merged(left, right);
-}
+// New impl - more concise
+const mergeSort = (nums) => {
+  if (nums.length <= 1) return nums;
+  const half = Math.floor(nums.length / 2);
+  const left = mergeSort(nums.slice(0, half));
+  const right = mergeSort(nums.slice(half));
 
-function merged(left, right) {
   let sorted = [];
-  let leftdup = left.slice();
-  let rightdup = right.slice();
-  while (leftdup.length > 0 && rightdup.length > 0) {
-    let firstLeft = leftdup[0];
-    let firstRight = rightdup[0];
-    if (firstLeft > firstRight) {
-      sorted.push(rightdup.shift());
-    } else {
-      sorted.push(leftdup.shift());
-    }
+  while (left.length > 0 && right.length > 0) {
+    sorted.push(left[0] > right[0] ? right.shift() : left.shift());
   }
-  return sorted.concat(leftdup, rightdup);
-}
+  return [...sorted, ...left, ...right];
+};
+
+// Old impl
+// function mergeSort(array) {
+//   if (array.length <= 1) return array;
+//   let mid = Math.floor(array.length / 2);
+//   let left = mergeSort(array.slice(0, mid));
+//   let right = mergeSort(array.slice(mid));
+//   return merged(left, right);
+// }
+
+// function merged(left, right) {
+//   let sorted = [];
+//   let leftdup = left.slice();
+//   let rightdup = right.slice();
+//   while (leftdup.length > 0 && rightdup.length > 0) {
+//     let firstLeft = leftdup[0];
+//     let firstRight = rightdup[0];
+//     if (firstLeft > firstRight) {
+//       sorted.push(rightdup.shift());
+//     } else {
+//       sorted.push(leftdup.shift());
+//     }
+//   }
+//   return sorted.concat(leftdup, rightdup);
+// }
 
 console.log(mergeSort([5, 4, 3, 1, 2]));
 // console.log(mergeSort([55, 123, 151, 6212, 7, 14, 13, 78, 1, 99]));
---
+++ rust/2_add_two_numbers/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    println!("hello world");
+}
+
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut carry = 0;
+        let (mut curr1, mut curr2) = (l1, l2);
+        let head = Some(Box::new(ListNode { val: 0, next: None }));
+        return head;
+    }
+}
