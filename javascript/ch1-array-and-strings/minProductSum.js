1bfec0d331a8bd0e9d2fe85777b2991a85adc717
David Chen <qydchen@gmail.com>
2024-02-01 19:43:51 -0800

    JS Medium LC1874 - minimize product sum of two arrays

---
+++ javascript/ch1-array-and-strings/minProductSum.js
@@ -0,0 +1,11 @@
+// LC 1874
+
+var minProductSum = function (nums1, nums2) {
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => b - a);
+  let ans = 0;
+  for (let i = 0; i < nums1.length; i++) {
+    ans += nums1[i] * nums2[i];
+  }
+  return ans;
+};
