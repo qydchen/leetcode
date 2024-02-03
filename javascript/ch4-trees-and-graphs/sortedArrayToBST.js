e3ab0a3dda586f71998bcd656449dd53847d45e3
David Chen <qydchen@gmail.com>
2024-02-03 01:07:05 -0800

    JS Easy LC 108 - Convert Sorted Array to BST

---
+++ javascript/ch4-trees-and-graphs/sortedArrayToBST.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+  if (!nums || nums.length == 0) return null;
+  let mid = Math.floor((nums.length - 1)/ 2);
+  let root = new TreeNode(nums[mid]);
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+  return root;
+};
