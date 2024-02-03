3d0266f1b59b6f485741842be6156b073043a7a7
David Chen <qydchen@gmail.com>
2024-02-03 00:37:30 -0800

    JS Easy LC101 - Symmetric Tree

--- javascript/ch4-trees-and-graphs/symmetricTree.js
+++ javascript/ch4-trees-and-graphs/symmetricTree.js
@@ -1,33 +1,25 @@
-// Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
-
-// For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
-
-//     1
-//    / \
-//   2   2
-//  / \ / \
-// 3  4 4  3
-
-// But the following [1,2,2,null,3,null,3] is not:
-
-//     1
-//    / \
-//   2   2
-//    \   \
-//    3    3
-
-const isSymmetric = function (root) {
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
   if (!root) return true;
-  const { left, right } = root;
-  return checkSymmetry(left, right);
+  return recurse(root.left, root.right);
 };
 
-function checkSymmetry(root1, root2) {
-  if ((root1 && !root2) || (!root1 && root2)) return false;
-  if (root1 === null && root2 === null) return true;
-  if (root1.val !== root2.val) return false;
-  return (
-    checkSymmetry(root1.left, root2.right) &&
-    checkSymmetry(root1.right, root2.left)
-  );
+const recurse = (left, right) => {
+  if (left == null && right == null) return true;
+  if ((!left && right) || (left && !right)) return false;
+  if (left.val !== right.val) return false;
+  const isLLsameAsRR = recurse(left.left, right.right);
+  const isLRsameAsRL = recurse(left.right, right.left);
+  return isLLsameAsRR && isLRsameAsRL;
 }
