85eb863bd26ca9dc6eb5d50b39a141bf039e62a0
David Chen <qydchen@gmail.com>
2025-05-16 00:12:35 -0400

    make a package.json for testing

--- javascript/basics/hashmap-modern.js
+++ javascript/basics/hashmap-modern.js
@@ -3,7 +3,6 @@ class HashMap {
   #count = 0;
   #capacity = 4;
 
-  // we'll use this as a helper function to analyze the internal behavior
   get diagnostics() {
     return {
       storage: JSON.stringify(this.#storage),
@@ -13,83 +12,42 @@ class HashMap {
   }
 
   set(key, value) {
-    //create an index for our storage location by passing it through our hashing function
-    let index = this.hashFunc(key, this.#capacity);
-    //get the bucket at this particular index in our storage, if one exists
-    //[[ [k,v], [k,v], [k,v] ] , [ [k,v], [k,v] ]  [ [k,v] ] ]
-    //does a bucket exist or do we get undefined when trying to get said index?
-    if (!this.#storage[index]) {
-      //create the bucket
-      //set the bucket into our HashMap
-      this.#storage[index] = [];
-    }
+    const index = this.hashFunc(key, this.#capacity);
+    const bucket = (this.#storage[index] = this.#storage[index] || []);
 
-    let bucket = this.#storage[index];
-
-    //now iterate through our bucket to see if there are any conflicting
-    //key value pairs within our bucket. If there are any, override them.
-    for (let i = 0; i < bucket.length; i++) {
-      let pair = bucket[i];
+    for (const pair of bucket) {
       if (pair[0] === key) {
-        //overide value stored at this key
         pair[1] = value;
         return;
       }
     }
 
-    // create a new pair in our bucket
-    // At this point, this could either be the new empty bucket we created above
-    // or a bucket with other pairs with keys that are different than
-    // the key of the pair we are inserting. These pairs are in the
-    // same bucket because their keys all equate to the same numeric index when
-    // passing through our hash function.
     bucket.push([key, value]);
-    this.#count++;
-    // now that we've added our new key/val pair to our storage
-    // let's check to see if we need to resize our storage
-    if (this.#count >= this.#capacity) {
-      this.resize(this.#capacity * 2);
-    }
-    return;
+    if (++this.#count >= this.#capacity) this.resize(this.#capacity * 2);
   }
 
   delete(key) {
-    let index = this.hashFunc(key, this.#capacity);
-    let bucket = this.#storage[index];
-    if (!bucket) {
-      return null;
-    }
-    //iterate over the bucket
-    for (let i = 0; i < bucket.length; i++) {
-      let pair = bucket[i];
-      //check to see if key is inside bucket
-      if (pair[0] === key) {
-        //if it is, get rid of this pair
-        this.#storage[index] = [...bucket.slice(0, i), ...bucket.slice(i + 1)];
-        this.#count--;
+    const index = this.hashFunc(key, this.#capacity);
+    const bucket = this.#storage[index];
+    if (!bucket) return null;
 
-        // this is a bonus, but we will resize down when there are enough deletions
-        if (this.#count < this.#capacity / 0.5) {
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        const [pair] = bucket.splice(i, 1);
+        if (--this.#count < this.#capacity / 0.5)
           this.resize(this.#capacity / 2);
-        }
         return pair;
       }
     }
   }
 
   get(key) {
-    let index = this.hashFunc(key, this.#capacity);
-    let bucket = this.#storage[index];
+    const index = this.hashFunc(key, this.#capacity);
+    const bucket = this.#storage[index];
+    if (!bucket) return null;
 
-    if (!bucket) {
-      return null;
-    }
-
-    for (let i = 0; i < bucket.length; i++) {
-      let pair = bucket[i];
-      if (pair[0] === key) {
-        return pair[1];
-      }
+    for (const pair of bucket) {
+      if (pair[0] === key) return pair[1];
     }
 
     return null;
@@ -97,30 +55,25 @@ class HashMap {
 
   hashFunc(str, max) {
     let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-      let letter = str[i];
-      hash = (hash << 5) + letter.charCodeAt(0);
-      hash = (hash & hash) % max;
+    for (const letter of str) {
+      hash = ((hash << 5) + letter.charCodeAt(0)) & hash;
     }
-    return hash;
+    return Math.abs(hash) % max;
   }
 
   resize(newLimit) {
-    let oldStorage = this.#storage;
-
+    const oldStorage = this.#storage;
     this.#capacity = newLimit;
     this.#count = 0;
     this.#storage = [];
 
-    oldStorage.forEach((bucket) => {
-      if (!bucket) {
-        return;
+    for (const bucket of oldStorage) {
+      if (bucket) {
+        for (const [key, value] of bucket) {
+          this.set(key, value);
+        }
       }
-      for (let i = 0; i < bucket.length; i++) {
-        let pair = bucket[i];
-        this.set(pair[0], pair[1]);
-      }
-    });
+    }
   }
 }
 
@@ -139,3 +92,5 @@ map.set("bar", 11);
 console.log(map.diagnostics);
 map.set("baz", 22);
 console.log(map.diagnostics);
+
+module.exports = HashMap;
